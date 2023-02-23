f228d82156ceed519546dd5a9f6a4077170184c9
Sam Green <samueltylergreen@gmail.com>
2023-02-23 15:10:04 -0800

    update gems

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,4 +1,4 @@
-Ruby::OpenAI.configure do |config|
+OpenAI.configure do |config|
         config.access_token = ENV.fetch('OPEN_AI')
         #config.organization_id = ENV.fetch('OPENAI_ORGANIZATION_ID') # Optional.
     end
