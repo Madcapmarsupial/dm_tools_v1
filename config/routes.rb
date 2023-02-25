61ece04ce29d3a357dcad4ac36d7c2f144d55959
Sam Green <samueltylergreen@gmail.com>
2023-02-24 18:13:51 -0800

    add home and landing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   #root "quests#index"
+   root to: "home#landing_page"
 
    resources :quests, only: [:create, :new, :show] do 
     member do
